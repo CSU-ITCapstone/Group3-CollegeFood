eeb2ff00eb79367d5c32807d90c1009e97a9d484
Forest McDonald <forest383@gmail.com>
2023-04-04 19:31:41 -0400

    Trying to connect the api. A basic html template

--- app.py
+++ app.py
@@ -6,6 +6,7 @@
 # Imports
 from flask import Flask
 from flask import render_template
+import urllib.request, json
 
 app = Flask(__name__)
 
@@ -13,5 +14,24 @@ app = Flask(__name__)
 def home():
     return "Columbus State Nutrition"
 
+@app.route("/apiTesting")
+def api_testing():
 
+    # URL for connecting to Step 1 of the Api, includes app id and api key
+    url = "https://api.edamam.com/api/food-database/v2/parser?app_id=d84791b8&app_key=498065e3b390e613e11cc5d5424eebce"
+
+    # opens the URL
+    response = urllib.request.urlopen(url)
+    # reads the data
+    data = response.read()
+    # stores the data in a dictionary
+    dict = json.loads(data)
+
+    # calles the html file apiTesting.html not sure what is happening after that yet. UPDATE 
+    # It doesn't give an error yet but it will load just with nothing on screen
+    return render_template ("apiTesting.html", parsed = dict["parsed"])
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
 
