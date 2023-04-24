837c62d3fd936d1296c4f3127ade3c13112026a7
NyeishaJPace <nyeisha.j.pace@gmail.com>
2023-04-24 04:53:01 +0000

    second card is working

--- templates/script.js
+++ templates/script.js
@@ -1,5 +1,10 @@
-var flipCard = document.getElementById('myFlipCard', 'myFlipCard2');
+var flipCard = document.getElementById('myFlipCard');
+var flipCard2 = document.getElementById('myFlipCard2');
 
 flipCard.addEventListener('click', function(){
     flipCard.classList.toggle('flip')
+});
+
+flipCard2.addEventListener('click', function(){
+    flipCard2.classList.toggle('flip')
 });
