abff9e52a2117a88b03e45138cafd11b0df44586
NyeishaJPace <nyeisha.j.pace@gmail.com>
2023-04-24 05:03:06 +0000

    added third card

--- templates/script.js
+++ templates/script.js
@@ -1,5 +1,6 @@
 var flipCard = document.getElementById('myFlipCard');
 var flipCard2 = document.getElementById('myFlipCard2');
+var flipCard3 = document.getElementById('myFlipCard3');
 
 flipCard.addEventListener('click', function(){
     flipCard.classList.toggle('flip')
@@ -7,4 +8,8 @@ flipCard.addEventListener('click', function(){
 
 flipCard2.addEventListener('click', function(){
     flipCard2.classList.toggle('flip')
+});
+
+flipCard3.addEventListener('click', function(){
+    flipCard3.classList.toggle('flip')
 });
